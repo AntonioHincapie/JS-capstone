ff3643df54590dbb56f0a17901f86d766d5fa838
AntonioHincapie <marcohincapiem@gmail.com>
2022-06-02 20:33:20 -0500

    Modify main structure and add popUp function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
 import './style.css';
 import Logo from './img/images.png';
-import catchPokemon from './module/comment.js';
+import { printPokemons } from './module/homepage.js';
+import { showComment } from './module/comment.js';
 
 const headLogo = document.getElementById('logo');
 const myLogo = new Image();
 myLogo.src = Logo;
 headLogo.appendChild(myLogo);
 
-const pokeContainer = document.getElementById('poke_container');
-const pokemonsNumber = 9;
-
-const createPokemonCard = (pokemon) => {
-  const pokemonEl = document.createElement('div');
-  pokemonEl.classList.add('pokemon');
-  const { name, sprites } = pokemon;
-  const pokeInnerHTML = `
-    <div class="img-container">
-      <img id="pokeimg" src="${sprites.other['official-artwork'].front_default}" alt="${name}" />
-      <h3 class="name">${name}</h3>
-      <button id="comments">comments</button>
-      <button class="reservations">Reservations</button>
-  
-    </div>
-    `;
-  pokemonEl.innerHTML = pokeInnerHTML;
-  pokeContainer.appendChild(pokemonEl);
+const popUp = async ()=> {
+  await printPokemons();
+  const commetBtn = document.querySelectorAll('.comments');
+  commetBtn.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+      showComment(e);
+    });
+  });
 };
 
-const getPokemon = async (id) => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const res = await fetch(url);
-  const pokemon = await res.json();
-  createPokemonCard(pokemon);
-};
-
-const fetchPokemons = async () => {
-  for (let i = 1; i <= pokemonsNumber; i += 1) {
-    getPokemon(i);
-    catchPokemon(i);
-  }
-};
-
-fetchPokemons();
-
-export default createPokemonCard;
+printPokemons();
+popUp();

ade9af0f8742ac43a931a0901e7525a3b6f8e209
franclobo <fjbl2788@gmail.com>
2022-06-02 20:40:47 -0500

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Logo from './img/images.png';
+import catchPokemon from './module/comment.js';
 
 const headLogo = document.getElementById('logo');
 const myLogo = new Image();
@@ -12,50 +13,18 @@ const pokemonsNumber = 9;
 const createPokemonCard = (pokemon) => {
   const pokemonEl = document.createElement('div');
   pokemonEl.classList.add('pokemon');
-  const { name, sprites, species, height, weight, id } = pokemon;
+  const { name, sprites } = pokemon;
   const pokeInnerHTML = `
     <div class="img-container">
       <img id="pokeimg" src="${sprites.other['official-artwork'].front_default}" alt="${name}" />
       <h3 class="name">${name}</h3>
-      <button class="comments">comments</button>
+      <button id="comments">comments</button>
       <button class="reservations">Reservations</button>
   
     </div>
     `;
   pokemonEl.innerHTML = pokeInnerHTML;
   pokeContainer.appendChild(pokemonEl);
-
-
-  const reserveBtn = document.querySelectorAll('.reservations');
-  const pokeReservation = document.querySelector('.poke_reservation');
-  //Display reservations pop up with selected item's details
-  let createReserve = () => {
-    const reserveEl = document.createElement('div');
-    reserveEl.classList.add('poke');
-    const reserveInnerHTML = `
-        <div class="img-container">
-          <img id="pokeimg" src="${sprites.other['official-artwork'].front_default}" alt="${name}" />
-          <h3 class="name">${name}</h3>
-          <ul class="atributes">
-            <li>Specie: ${species.name}</li>
-            <li>Height: ${height}</li>
-            <li>Weight: ${weight}</li>
-            <li>ID: ${id}</li>
-          </ul>
-        </div>
-        
-        `;
-    reserveEl.innerHTML = reserveInnerHTML;
-    pokeReservation.appendChild(reserveEl);
-    reserveBtn.forEach((element) => {
-      element.addEventListener('click', () => {
-
-        console.log('ditto');
-
-      });
-    });
-  };
-  
 };
 
 const getPokemon = async (id) => {
@@ -68,7 +37,10 @@ const getPokemon = async (id) => {
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokemonsNumber; i += 1) {
     getPokemon(i);
+    catchPokemon(i);
   }
 };
 
 fetchPokemons();
+
+export default createPokemonCard;

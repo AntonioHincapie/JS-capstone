35e76ff477b65fab72e9244e932480db29e339ff
franclobo <fjbl2788@gmail.com>
2022-06-03 15:14:48 -0500

    Update reservations.js

--- src/module/reservations.js
+++ src/module/reservations.js
@@ -100,6 +100,13 @@ const showReservation = async (e) => {
         <h3 class="title">Reservations (2)</h3><br>
         <div id="allreservations"></div>
       </div>
+       <form class="reservation-form">
+        <h3 class="title">Add a reservation</h3><br>
+        <input class="name" type:"text" placeholder="Your name" autofocus><br>
+        <input class="date_start" type:"date" placeholder="Start date"><br>
+        <input class="date_finish" type:"date" placeholder="End date"><br>
+        <button class="addReservation">Reserve</button>     
+      </form>
     </div>
   `);
   hideReservation();

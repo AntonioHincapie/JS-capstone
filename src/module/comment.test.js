d5d48cc3b26bf0da2835e9003986e311e19632aa
AntonioHincapie <marcohincapiem@gmail.com>
2022-06-04 09:06:15 -0500

    Add comment counter test.

---
+++ src/module/comment.test.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+describe('Counter of comments', () => {
+  beforeAll(() => {
+    document.body.innerHTML = fs.readFileSync('dist/index.html');
+  });
+  test('Number of comments', () => {
+    const popUp = document.getElementById('commentPopup');
+    const counter = document.createElement('h3');
+    const commentCouter = (comment) => {
+      popUp.appendChild(counter);
+      counter.innerHTML = `Comments (${comment.length})`;
+    };
+    commentCouter(['C1', 'C2', 'C3', 'C4']);
+    expect(counter.innerHTML).toEqual('Comments (4)')
+  });
+});
